ea1935388f3deb2601a588b5805f29f7d05d1cd1
David <dhahn123@gmail.com>
2019-04-05 17:59:31 -0700

    setup Heroku Production in server.js

---
+++ client/build/precache-manifest.2e3f4f2f1aeb891716e7051535dd0443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae811cc04dc85bb351cb",
+    "url": "/static/css/main.b0ba6794.chunk.css"
+  },
+  {
+    "revision": "ae811cc04dc85bb351cb",
+    "url": "/static/js/main.c5fe744d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49b550564ddb20555899",
+    "url": "/static/js/2.1fb3b6ae.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3f67b46adec0867f12d243985ded797",
+    "url": "/index.html"
+  }
+];
