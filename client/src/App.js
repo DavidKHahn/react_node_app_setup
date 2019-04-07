d6aeb4cee544a9d28d70ddb6ca4cc4d66e71c316
David <dhahn123@gmail.com>
2019-04-06 23:23:06 -0700

    Install react-router-dom and set up components: About, Contact, Home, NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   state = {
@@ -36,6 +37,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
